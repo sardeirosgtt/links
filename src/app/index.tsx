4d13e43143fac856c830e2746def857632b37fc3
Asp Sardeiro <1117864775@sef2.eb.mil.br>
2025-05-28 16:26:02 -0300

    index.tsx

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import{Text, View} from "react-native"
+
+export default function Index(){
+    return(
+
+        <view>
+        <Text>Hello React Native</Text>
+        <Text>Hello</Text>
+        </view>
+
+    )
+}
