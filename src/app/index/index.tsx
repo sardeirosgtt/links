04664ebeecb53827cb105f0db426208ca88776ba
Asp Sardeiro <1117864775@sef2.eb.mil.br>
2025-05-29 16:05:53 -0300

    teste

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,18 +1,53 @@
-import { Image, View, TouchableOpacity } from "react-native";
+import { Image, View, TouchableOpacity, FlatList, Modal, Text } from "react-native";
 import { styles } from "./styles";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
+import { Categories } from "@/components/categories";
+import Link from "@/components/link";
+import Option from "@/components/option";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Image source={require("@/app/assets/logo.png")} style={styles.logo} />
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
         <TouchableOpacity>
           <MaterialIcons name="add" size={44} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+
+      <Categories />
+
+      <FlatList data={["1", "2", "3", "4", "5"]} keyExtractor={item => item} renderItem={() => (
+        <Link name="Rocketseat" url="https://www.rocketseat.com.br" onDetails={() => console.log("clicou")} />
+      )}
+        style={styles.links}
+        contentContainerStyle={styles.linksContent}
+        showsVerticalScrollIndicator={false}
+      />
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}> Curso </Text>
+              <TouchableOpacity>
+              <MaterialIcons name="close" size={20} color={colors.gray[400]} />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.modalLinkName}>
+              Rocketseat </Text>
+            <Text style={styles.modalUrl}>
+              https://www.rocketseat.com.br </Text>
+              <View style={styles.modalFooter} >
+                <Option name="Excluir" icon="delete" variant="secondary"/>
+                <Option name="Abrir" icon="language"/>
+              </View>
+          </View>
+        </View>
+      </Modal>
+
     </View>
   );
 }
