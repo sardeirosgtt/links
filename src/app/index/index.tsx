ad15505091f63530e8c63e1de748143509177d7c
sardeiro <ddandre1010@gmail.com>
2025-05-28 23:38:32 -0300

    avançando no curso

---
+++ src/app/index/index.tsx
@@ -0,0 +1,18 @@
+import { Image, View, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { colors } from "@/styles/colors";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("@/app/assets/logo.png")} style={styles.logo} />
+
+        <TouchableOpacity>
+          <MaterialIcons name="add" size={44} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
